131232015ffeb8629f0eaacf12d98b9ae87df368
Andrei Zlate-Podani <podani@avast.com>
2017-10-23 16:41:45 +0200

    uint16_t with partitions

--- radix/radix3.cpp
+++ radix/radix3.cpp
@@ -90,26 +90,26 @@ struct IdentityKey {
 // }
 
 
-// struct ExtractHighByte {
-	// uint8_t operator()(const uint16_t& val) {
-		// return val>>8;
-	// }
+struct ExtractHighByte {
+	uint8_t operator()(const uint16_t& val) {
+		return val>>8;
+	}
 	
-	// uint8_t operator()(const int16_t& val) {
-		// return ((uint16_t)val + 0x8000)>>8;	// can do better
-	// }
-// };
+	uint8_t operator()(const int16_t& val) {
+		return ((uint16_t)val + 0x8000)>>8;	// can do better
+	}
+};
 
 
-// struct ExtractLowByte {
-	// uint8_t operator()(const uint16_t& val) {
-		// return val&0xFF;
-	// }
+struct ExtractLowByte {
+	uint8_t operator()(const uint16_t& val) {
+		return val&0xFF;
+	}
 	
-	// uint8_t operator()(const int16_t& val) {
-		// return ((uint16_t)val + 0x8000)&0xFF;	// can do better
-	// }
-// };
+	uint8_t operator()(const int16_t& val) {
+		return ((uint16_t)val + 0x8000)&0xFF;	// can do better
+	}
+};
 
 
 // void radix_uint16(uint16_t* first, uint16_t* last) {
@@ -147,7 +147,7 @@ struct partition_t {
 
 
 template <typename T, typename ExtractKey>
-void update_counts(std::array<partition_t, 256>& partitions, T first, T last, ExtractKey& ek)
+void update_counts(std::array<partition_t, 256>& partitions, T first, T last, int, ExtractKey& ek)
 {
 	for (auto it = first; it != last; ++it) {
 		++partitions[ek(*it)].count;
@@ -210,7 +210,7 @@ int reduce_chain(std::array<partition_t, 256>& partitions, int chain_start, cons
 
 
 template <typename T, typename ExtractKey>
-void apply_prefixes(T first, std::array<partition_t, 256>& partitions, int chain_start, const std::array<int32_t, 256>& prefix, ExtractKey& ek)
+void apply_prefixes(T first, std::array<partition_t, 256>& partitions, int chain_start, const std::array<int32_t, 256>& prefix, int, ExtractKey& ek)
 {
 	using std::swap;
 	bool sorted = true;
@@ -241,18 +241,96 @@ void apply_prefixes(T first, std::array<partition_t, 256>& partitions, int chain
 
 
 template <typename T, typename ExtractKey>
-void radix_byte_p(T first, T last, ExtractKey& ek)
+void radix_byte_p(T first, T last, int, ExtractKey& ek)
 {
 	std::array<partition_t, 256> count = {0};
 	
-	update_counts(count, first, last, ek);
+	update_counts(count, first, last, 0, ek);
 	
 	std::array<int32_t, 256> prefix;
 
 	auto start = compute_prefixes(count, prefix);
 	
-	apply_prefixes(first, count, start, prefix, ek);
+	apply_prefixes(first, count, start, prefix, 0, ek);
 }
+
+
+std::array<std::pair<int32_t, int32_t>, 257> fill_recurse_table(const std::array<partition_t, 256>& partitions) {
+	std::array<std::pair<int32_t, int32_t>, 257> recurse_table;
+	
+	int32_t start = 0;
+	int pos = 0;
+	for (int i=0; i<256; ++i) {
+		if (partitions[i].count > 1) {
+			recurse_table[pos++] = std::make_pair(start, start+partitions[i].count);
+		}
+		
+		start += partitions[i].count;
+	}
+	
+	recurse_table[pos].second = 0;
+	
+	return recurse_table;
+}
+
+
+// void radix_uint16_p(uint16_t* first, uint16_t* last) {
+	// using std::swap;
+	// std::array<partition_t, 256> count = {0};
+	
+	// update_counts(count, first, last, 0, ExtractHighByte());
+	
+	// auto recurse_table = fill_recurse_table(count);
+	// std::array<int32_t, 256> prefix;
+
+	// auto start = compute_prefixes(count, prefix);
+	
+	// apply_prefixes(first, count, start, prefix, 0, ExtractHighByte());
+	
+	// for (int i=0; recurse_table[i].second; ++i) {
+		// if (recurse_table[i].second - recurse_table[i].first < 128) {
+			// std::sort(first+recurse_table[i].first, first+recurse_table[i].second, [](uint16_t left, uint16_t right) {
+				// ExtractLowByte ek;
+				// return ek(left) < ek(right);
+			// });
+		// }
+		// else {
+			// radix_byte_p(first+recurse_table[i].first, first+recurse_table[i].second, 0, ExtractLowByte());
+		// }
+	// }
+// }
+
+void radix_uint16_p(uint16_t* first, uint16_t* last) {
+	using std::swap;
+	std::array<partition_t, 256> count = {0};
+	
+	update_counts(count, first, last, 0, ExtractHighByte());
+	
+	std::array<int32_t, 256> prefix;
+
+	auto start = compute_prefixes(count, prefix);
+	
+	apply_prefixes(first, count, start, prefix, 0, ExtractHighByte());
+	
+	int32_t pos = 0;
+	int32_t old_prefix = 0;
+
+	for (int i=0; i<256; ++i) {
+		/*if (prefix[i]-old_prefix < 128) {
+			std::sort(first+pos, first+prefix[i], [](uint16_t left, uint16_t right) {
+				ExtractLowByte ek;
+				return ek(left) < ek(right);
+			});
+		}
+		else*/ {
+			radix_byte_p(first+pos, first+prefix[i], 0, ExtractLowByte());
+		}
+		
+		pos += prefix[i] - old_prefix;
+		old_prefix = prefix[i];
+	}
+}
+
 
 
 void radix_string(std::string& first, std::string& last) {
@@ -260,8 +338,8 @@ void radix_string(std::string& first, std::string& last) {
 
 
 void main() {
-	std::vector<uint8_t> vec;
-	//std::vector<uint16_t> vec;//={1,5,4,0,0};
+	//std::vector<uint8_t> vec;
+	std::vector<uint16_t> vec;//={1,5,4,0,0};
 	//std::vector<int16_t> vec;//={1,5,4,0,0};
 	// uint8_t vec[256] = {128, 15, 77, 127, 126, 0, 0, 125, 124, 116,
 						// 207, 123, 122, 0, 0, 121, 120, 0, 0, 119,
@@ -308,7 +386,7 @@ void main() {
 	//		[urg=std::uniform_int_distribution<int16_t>(std::numeric_limits<int16_t>::min()), &g]{
 	//			return urg(g);
 	//		});
-	auto x = std::uniform_int_distribution<int>(0,255);
+	auto x = std::uniform_int_distribution<int>(0,0xFFFF);
 	 std::generate_n(std::back_inserter(vec), 1000000, 
 			[urg=x, &g]{
 				return urg(g);
@@ -319,10 +397,10 @@ void main() {
 	 for (int i=0; i<15; ++i) {
 		 std::shuffle(vec.begin(), vec.end(), g);
 		 QueryPerformanceCounter(&li);
-		 //radix_uint16(&vec[0], &vec[0]+vec.size());
-		 radix_byte_p(&vec[0], &vec[0]+vec.size(), IdentityKey());
+		 //radix_uint16_p(&vec[0], &vec[0]+vec.size());
+		 //radix_byte_p(&vec[0], &vec[0]+vec.size(), 0, IdentityKey());
 		 //std::sort(&vec[0], &vec[0]+vec.size());
-		 //ska_sort(&vec[0], &vec[0]+vec.size());
+		 ska_sort(&vec[0], &vec[0]+vec.size());
 		 QueryPerformanceCounter(&li2); time += li2.QuadPart - li.QuadPart;
 	 }
 
